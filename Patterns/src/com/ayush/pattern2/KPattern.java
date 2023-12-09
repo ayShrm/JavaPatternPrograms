40089186ab5a751c654b44276aaf9bcda168d8bf
ayShrm <aaysh3256@gmail.com>
2023-12-09 19:49:31 +0530

    Initial commit

--- Patterns/src/com/ayush/pattern2/KPattern.java
+++ Patterns/src/com/ayush/pattern2/KPattern.java
@@ -1,10 +1,34 @@
 package com.ayush.pattern2;
 
+import java.util.Scanner;
+
 public class KPattern {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of rows: ");
+		int row = sc.nextInt();
+
+		// FirstHalf
+		for (int i = 1; i <= row; i++) {
+
+			for (int j = row; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = row - 1; i >= 1; i--) {
+
+			for (int j = row; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		sc.close();
 	}
 
 }
--- Patterns/src/com/ayush/pattern2/PascalTriangle.java
+++ Patterns/src/com/ayush/pattern2/PascalTriangle.java
@@ -1,10 +1,33 @@
 package com.ayush.pattern2;
 
+import java.util.Scanner;
+
 public class PascalTriangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number of rows: ");
+		int row = sc.nextInt();
+
+		// FirstHalf
+		for (int i = 1; i <= row; i++) {
+
+			for (int j = 1; j <= row - i; j++) {
+				System.out.print(" ");
+			}
+			int num = 1;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(num + " ");
+				num = num * (i - j) / (j);
+			}
+
+			System.out.println();
+		}
+
+		sc.close();
 	}
 
 }
